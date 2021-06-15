fcf883235cfbbbbb996e78395ebb0baa2f9eacf2
zhongshenglong <1156420815@qq.com>
2021-06-15 19:52:18 +0800

    合并两个有序链表

---
+++ src/main/java/com/fightzhong/lc_0021_MergeTwoSortedLists.java
@@ -0,0 +1,35 @@
+package com.fightzhong;
+
+public class lc_0021_MergeTwoSortedLists {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode();
+        ListNode cur = dummyHead;
+        while ( l1 != null || l2 != null ) {
+            if ( l1 == null ) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else if ( l2 == null ) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else if ( l1.val < l2.val ) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+
+            cur = cur.next;
+        }
+
+        return dummyHead.next;
+    }
+}
