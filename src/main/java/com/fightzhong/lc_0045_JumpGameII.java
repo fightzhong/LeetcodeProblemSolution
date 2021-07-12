9b7a9cebdf70367a0ffd84edcb28eb8b8e092c44
zhongshenglong <1156420815@qq.com>
2021-07-12 21:51:00 +0800

    跳跃游戏 II

---
+++ src/main/java/com/fightzhong/lc_0045_JumpGameII.java
@@ -0,0 +1,21 @@
+package com.fightzhong;
+
+public class lc_0045_JumpGameII {
+    public int jump(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[nums.length - 1] = 0;
+
+        for ( int i = nums.length - 2; i >= 0; i -- ) {
+            int min = Integer.MAX_VALUE;
+            for ( int j = 1; j <= nums[i]; j ++ ) {
+                int value = dp[i + j] + 1;
+                value = value < 0 ? Integer.MAX_VALUE : value;
+                min = Math.min( min, value );
+            }
+
+            dp[i] = min;
+        }
+
+        return dp[0];
+    }
+}
