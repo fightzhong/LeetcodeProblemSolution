eb60f07589f9563f8fd50ff326f714bc7498b661
zhongshenglong <1156420815@qq.com>
2021-07-12 22:57:55 +0800

    全排列

---
+++ src/main/java/com/fightzhong/lc_0046_Permutations.java
@@ -0,0 +1,45 @@
+package com.fightzhong;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class lc_0046_Permutations {
+    public static void main(String[] args) {
+        new lc_0046_Permutations().permute( new int[]{1, 2, 3} );
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        return permute( nums, 0 );
+    }
+
+    public List<List<Integer>> permute(int[] nums, int index) {
+        List<List<Integer>> result = new ArrayList<>();
+        if ( index == nums.length - 1 ) {
+            List<Integer> data = new ArrayList<>();
+            data.add( nums[index] );
+            result.add( data );
+            return result;
+        }
+
+        for ( int i = index; i < nums.length; i ++ ) {
+            int temp = nums[i];
+            nums[i] = nums[index];
+            nums[index] = temp;
+
+            List<List<Integer>> data = permute( nums, index + 1 );
+            for ( List<Integer> item: data ) {
+                item.add( nums[index] );
+            }
+
+            result.addAll( data );
+
+            temp = nums[i];
+            nums[i] = nums[index];
+            nums[index] = temp;
+        }
+
+        return result;
+    }
+}
