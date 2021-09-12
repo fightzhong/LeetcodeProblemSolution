8a901cfaffeaa8620d42f830f5025f01783d60d3
zhongshenglong <1156420815@qq.com>
2021-09-12 14:00:16 +0800

    二叉树的前序遍历

--- src/main/java/com/fightzhong/question_101_to_150/lc_0144_BinaryTreePreorderTraversal.java
+++ src/main/java/com/fightzhong/question_101_to_150/lc_0144_BinaryTreePreorderTraversal.java
@@ -1,6 +1,7 @@
 package com.fightzhong.question_101_to_150;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class lc_0144_BinaryTreePreorderTraversal {
@@ -10,6 +11,29 @@ public class lc_0144_BinaryTreePreorderTraversal {
             result.add( root.val );
             result.addAll( preorderTraversal( root.left ) );
             result.addAll( preorderTraversal( root.right ) );
+        }
+
+        return result;
+    }
+
+    // 迭代解法
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        if ( root != null ) {
+            stack.addLast( root );
+            while ( stack.size() != 0 ) {
+                TreeNode node = stack.removeLast();
+                result.add( node.val );
+
+                if ( node.right != null ) {
+                    stack.addLast( node.right );
+                }
+
+                if ( node.left != null ) {
+                    stack.addLast( node.left );
+                }
+            }
         }
 
         return result;
